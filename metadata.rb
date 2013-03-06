971efd0ccd3b25cb02efdfe161425934b6560a94
John Dewey <john@dewey.ws>
2013-03-05 22:43:53 -0800

    some cleanups to match our other cookbooks

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,9 @@ maintainer        "Abel Lopez"
 maintainer_email  "alop@att.com"
 license           "Apache 2.0"
 description       "Configures bash"
-version           "0.0.1"
+version           "0.0.2"
+
+recipe            "raid", "Installs/Configures bash"
 
 %w{ubuntu debian centos redhat amazon scientific fedora}.each do |os|
   supports os
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,20 @@
 # Cookbook Name:: bash
 # Recipe:: default
 #
+# Copyright 2012-2013, Abel Lopez
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 zone = node["fqdn"].split(".")[2]
 
@@ -11,31 +25,33 @@ end
 
 template "/root/.bashrc" do
   source "bashrc.erb"
-  mode 0644
-  owner "root"
-  group "root"
-  variables({
+  owner  "root"
+  group  "root"
+  mode   00644
+
+  variables(
     :zone_name => zone
-  })
+  )
 end
 
 template "/etc/bash.bashrc" do
   source "bash.bashrc.erb"
-  mode 0644
-  owner "root"
-  group "root"
-  variables({
+  owner  "root"
+  group  "root"
+  mode   00644
+
+  variables(
     :zone_name => zone
-  })
+  )
 end
 
 template "/etc/skel/.bashrc" do
   source "bashrc.erb"
-  mode 0644
-  owner "root"
-  group "root"
-  variables({
-    :zone_name => zone
-  })
-end
+  owner  "root"
+  group  "root"
+  mode   0644
 
+  variables(
+    :zone_name => zone
+  )
+end
