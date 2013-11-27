a21acfcafbd9bc53feecf260d1be05c278c60aae
alop <alopgeek@gmail.com>
2013-11-27 08:49:13 -0800

    export EDITOR=vi
    nuff said
    
    Change-Id: Ib17a4ec049b5a976d9dfc821bb8deb294089304a

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Abel Lopez"
 maintainer_email  "alop@att.com"
 license           "Apache 2.0"
 description       "Configures bash"
-version           "0.0.5"
+version           "0.0.6"
 
 recipe            "bash", "Installs/Configures bash"
 
